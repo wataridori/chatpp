e472dcadc6801fc1775f2355329e714c160f9945
Kenini1805 <huusu1996@gmail.com>
2018-04-01 11:38:35 +0700

    laravel_mix_js

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+var mix = require('laravel-mix');
+
+mix.js([
+        'src/js/libraries/bootstrap.min.js',
+        'src/js/libraries/bootbox.min.js'
+    ], 'build/js/externals/libs.js')
+    .js('src/js/externals/popup.js', 'build/js/externals/popup.js')
+    .js([
+            'src/js/externals/emoticon.js',
+            'src/js/externals/shortcut.js',
+            'src/js/externals/notification.js',
+            'src/js/externals/room.js',
+            'src/js/externals/setting.js',
+            'src/js/externals/group.js',
+        ], 'build/js/externals/pages.js')
+    .js('src/js/extensions/contentscript.js', 'build/js/extensions/contentscript.js')
+    .js('src/js/extensions/background.js', 'build/js/extensions/background.js')
+    .js('src/js/internals/main.js', 'build/js/internals/all.js')
